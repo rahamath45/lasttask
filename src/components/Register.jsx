79b05b713068f5bb746116b923b844e53faeeb5f
rahamath45 <rahmathsulaiman45@gmail.com>
2025-09-18 16:10:47 +0530

    the url change

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -8,13 +8,12 @@ function Register() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [ role,setRole] = useState("")
   const [msg, setMsg] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("/api/auth/register", { name, email, password,role });
+      const res = await axios.post("/api/auth/register", { name, email, password });
               setMsg(res.data.message);
     } catch (err) {
       setMsg(err.response?.data?.message || "Something went wrong");
@@ -31,8 +30,6 @@ function Register() {
           value={email} onChange={(e) => setEmail(e.target.value)} required />
         <input className="form-control mb-2" type="password" placeholder="Password"
           value={password} onChange={(e) => setPassword(e.target.value)} required />
-          <input className="form-control mb-2" placeholder="Role"
-          value={role} onChange={(e) => setRole(e.target.value)} required />
         <button className="btn btn-primary w-100">Register</button>
       </form>
       {msg && <p className="mt-3">{msg}</p>}
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
 
-axios.defaults.baseURL = "http://localhost:8000"
+axios.defaults.baseURL = "https://passwordreset-1-r8eb.onrender.com"
 
 createRoot(document.getElementById('root')).render(
 <BrowserRouter>

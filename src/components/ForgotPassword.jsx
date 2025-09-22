8403e210cafd88d5e3f2d75a08bbafed86e90957
rahamath45 <rahmathsulaiman45@gmail.com>
2025-09-22 15:56:19 +0530

    the last

--- src/components/ForgotPassword.jsx
+++ src/components/ForgotPassword.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+
 
 function ForgotPassword() {
   const [email, setEmail] = useState("");
